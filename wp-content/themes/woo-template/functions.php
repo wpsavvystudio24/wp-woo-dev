5858ce79ab8b7bf9af0747bb7b96beaea118038e
benjoco16 <benjoco10@gmail.com>
2024-05-18 15:28:03 +0800

    Use Boilerplate and cleanup codes

--- wp-content/themes/woo-template/functions.php
+++ wp-content/themes/woo-template/functions.php
@@ -1,49 +1,45 @@
 <?php
-function theme_setup() {
-    // Add support for various theme features
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
+/**
+ * UnderStrap functions and definitions
+ *
+ * @package Understrap
+ */
 
-    // Register primary menu
-    register_nav_menus(array(
-        'primary' => __('Primary Menu', 'text_domain'),
-    ));
-}
-add_action('after_setup_theme', 'theme_setup');
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
-function enqueue_styles_and_scripts() {
-    // Enqueue styles
-    wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css');
-    wp_enqueue_style('core-styles', get_template_directory_uri() . '/css/styles.css');
-    wp_enqueue_style('custom-styles', get_template_directory_uri() . '/css/custom.css');
+// UnderStrap's includes directory.
+$understrap_inc_dir = 'inc';
 
-    // Enqueue scripts
-    wp_enqueue_script('bootstrap-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), null, true);
-    wp_enqueue_script('core-scripts', get_template_directory_uri() . '/js/scripts.js', array(), null, true);
-}
-add_action('wp_enqueue_scripts', 'enqueue_styles_and_scripts');
+// Array of files to include.
+$understrap_includes = array(
+	'/theme-settings.php',                  // Initialize theme default settings.
+	'/setup.php',                           // Theme setup and custom theme supports.
+	'/widgets.php',                         // Register widget area.
+	'/enqueue.php',                         // Enqueue scripts and styles.
+	'/template-tags.php',                   // Custom template tags for this theme.
+	'/pagination.php',                      // Custom pagination for this theme.
+	'/hooks.php',                           // Custom hooks.
+	'/extras.php',                          // Custom functions that act independently of the theme templates.
+	'/customizer.php',                      // Customizer additions.
+	'/custom-comments.php',                 // Custom Comments file.
+	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
+	'/editor.php',                          // Load Editor functions.
+	'/block-editor.php',                    // Load Block Editor functions.
+	'/deprecated.php',                      // Load deprecated functions.
+);
 
-// Add classes to nav menu items
-function add_additional_class_on_li($classes, $item, $args) {
-    if(isset($args->add_li_class)) {
-        $classes[] = $args->add_li_class;
-    }
-    return $classes;
-}
-add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
-
-function add_additional_class_on_a($classes, $item, $args) {
-    if (isset($args->link_class)) {
-        $classes['class'] = $args->link_class;
-    }
-    return $classes;
-}
-add_filter('nav_menu_link_attributes', 'add_additional_class_on_a', 1, 3);
-
-function mytheme_add_woocommerce_support() {
-    add_theme_support('woocommerce');
+// Load WooCommerce functions if WooCommerce is activated.
+if ( class_exists( 'WooCommerce' ) ) {
+	$understrap_includes[] = '/woocommerce.php';
 }
 
-add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
+// Load Jetpack compatibility file if Jetpack is activiated.
+if ( class_exists( 'Jetpack' ) ) {
+	$understrap_includes[] = '/jetpack.php';
+}
 
-?>
+// Include files.
+foreach ( $understrap_includes as $file ) {
+	require_once get_theme_file_path( $understrap_inc_dir . $file );
+}
--- wp-content/themes/woo-template/header.php
+++ wp-content/themes/woo-template/header.php
@@ -1,31 +1,38 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4' );
+$navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="<?php bloginfo('description'); ?>" />
-    <meta name="author" content="" />
-    <title><?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
-    <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon.ico" />
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="<?php echo home_url(); ?>">WP + Woocommerce Development</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'primary',
-                        'container' => false,
-                        'menu_class' => 'navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4',
-                        'add_li_class'  => 'nav-item',
-                        'link_class' => 'nav-link'
-                    ));
-                ?>
 
-            </div>
-        </div>
-    </nav>
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<header id="wrapper-navbar">
+
+		<a class="skip-link <?php echo understrap_get_screen_reader_class( true ); ?>" href="#content">
+			<?php esc_html_e( 'Skip to content', 'understrap' ); ?>
+		</a>
+
+		<?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
+
+	</header><!-- #wrapper-navbar -->
